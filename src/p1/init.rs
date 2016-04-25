b43fd408d79fbf5b0695f50e32e52182d3a66f12
Stephen Pearson <stephen@hpe.com>
2016-04-26 00:07:34 +0100

    Problem 1 complete

--- src/p1/init.rs
+++ src/p1/init.rs
@@ -1,5 +1,11 @@
 // Problem 1
 
 pub fn run(_: Vec<String>) {
-    println!("Hello World!");
+    let mut tot = 0;
+    for i in 1..1000 {
+        if ( i % 3 == 0 ) || ( i % 5 == 0 ) {
+            tot += i;
+        }
+    }
+    println!("Answer: {}", tot);
 }
